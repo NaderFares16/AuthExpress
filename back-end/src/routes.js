6158d59713bfb0e313bf06f008a68cb12cd51b1b
Nader Fares <naderalifares16@hotmail.com>
2024-11-09 21:58:40 -0300

    feat: authentication of credentials and default user

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,9 +1,24 @@
 const express = require('express');
 
 const routes = express.Router();
+const users = [
+  {
+    id: 1,
+    name: 'admin',
+    email: 'admin@auth.com',
+    password: 'admin'
+  }
+]
 
 routes.post('/login', (req, res) => {
-  res.send('Login endpoint');
+  const { email, password } = req.body;
+
+  const user = users.find(user => user.email === email && user.password === password);
+  if (user) {
+    res.status(200).json(user);
+  }
+
+ return res.status(401).json({message: 'Invalid Credentials'});
 });
 
 module.exports = routes;
