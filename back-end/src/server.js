934aa3930129d14d950ce55835902e92092ecf38
Nader Fares <naderalifares16@hotmail.com>
2024-11-09 21:45:34 -0300

    feat: Create routes and login endpoint

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const routes = require('./routes');
 
 app.use(express.json());
+app.use(routes);
 
 app.get('/', (req, res) => {
   res.send('Hello World');
@@ -9,4 +11,4 @@ app.get('/', (req, res) => {
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000 ✅');
-})
+});
